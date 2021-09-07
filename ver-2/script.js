b65b2f83b3822e8b4607b921323b21b4980081e2
Ivan Lesko <65163471+Ivanlsko@users.noreply.github.com>
2021-09-07 22:18:17 +0200

    ver 2 fully working with div square

--- ver-2/script.js
+++ ver-2/script.js
@@ -1,41 +1,44 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOMContentLoaded");
+  document.querySelector("input").addEventListener("input", convert);
+});
+
 //variables
-let hexCode;
-let RGB = [];
-let r;
-let g;
-let b;
-let h, s, l;
-inputToHEX();
 
 //gathering input data into hex variable
-function inputToHEX() {
+function convert() {
   console.log("Input to HEX");
-  document.querySelector("input").addEventListener("input", () => {
-    hexCode = document.querySelector("input").value;
-    console.log(hexCode);
-    hexToRGB();
-  });
+  let hexCode = document.querySelector("input").value;
+  console.log(hexCode);
+  let RGBCode = hextoRGB(hexCode);
+  let HSLCode = RGBtoHSL(RGBCode);
+  showTheValue(hexCode, RGBCode, HSLCode);
+  changeTheColor(RGBCode);
 }
 
 //converting hex to rgb
-function hexToRGB() {
+function hextoRGB(hexCode) {
   console.log("Hex to RGB");
-  r = hexCode.substring(1, 3);
+  let r = hexCode.substring(1, 3);
   r = parseInt(r, 16);
-  g = hexCode.substring(3, 5);
+  let g = hexCode.substring(3, 5);
   g = parseInt(g, 16);
-  b = hexCode.substring(5, 7);
+  let b = hexCode.substring(5, 7);
   b = parseInt(b, 16);
-  RGB = [r, g, b];
-  console.log(RGB);
-  RGBtoHSL();
+  let RGBarr = [r, g, b];
+  console.log(RGBarr);
+  return RGBarr;
 }
 
 //converting rgb to hsl
 //!! this function is not written by me
-function RGBtoHSL() {
+function RGBtoHSL(RGBCode) {
   console.log("Rgb to HSL");
+  let h, s, l;
 
+  let r = RGBCode[0];
+  let g = RGBCode[1];
+  let b = RGBCode[2];
   r /= 255;
   g /= 255;
   b /= 255;
@@ -68,20 +71,19 @@ function RGBtoHSL() {
   s *= 100;
   l *= 100;
   console.log("hsl(%f,%f%,%f%)", h, s, l); // just for testing
-  showTheValue();
+  return [h, s, l];
 }
 
 //function that shows the value of HEX. RGB, HSL in html
-function showTheValue() {
+function showTheValue(hexCode, RGB, HSL) {
   console.log("Show the values in html");
   document.querySelector("#HEX > p").innerHTML = hexCode;
   document.querySelector("#RGB > p").innerHTML = `${RGB[0]}, ${RGB[1]}, ${RGB[2]}`;
-  document.querySelector("#HSL > p").innerHTML = `${h}, ${Math.floor(s)}%, ${Math.floor(l)}%`;
-  changeTheColor();
+  document.querySelector("#HSL > p").innerHTML = `${Math.floor(HSL[0])}, ${Math.floor(HSL[1])}%, ${Math.floor(HSL[2])}%`;
 }
 
 //function that changes the color of the bigger div
-function changeTheColor() {
+function changeTheColor(RGB) {
   console.log("Change the color");
   document.querySelector("#square").style.backgroundColor = `rgb(${RGB[0]}, ${RGB[1]}, ${RGB[2]})`;
 }
